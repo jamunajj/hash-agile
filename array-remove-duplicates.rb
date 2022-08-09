e8dc0edf571a177ebbfe2c017bc52012bd0c83c4
Jamuna Janardhanan <jamunajjanardhanan@gmail.com>
2022-08-09 16:13:00 +0530

    Replaced core function uniq with loop function

--- array-remove-duplicates.rb
+++ array-remove-duplicates.rb
@@ -4,6 +4,11 @@ puts "Enter your list of numbers with space eg 10 45 80 60:"
 
 numbers = gets.chomp.split(" ").map(&:to_i)
 
-numbers.uniq!
+#numbers.uniq!
 
-numbers.each { |n| print n," "}
+numbers_unique = []
+
+numbers.each { |n| numbers_unique.push(n) if !numbers_unique.include? n }
+
+puts "Unique array values: "
+numbers_unique.each { |n| print n," " }
